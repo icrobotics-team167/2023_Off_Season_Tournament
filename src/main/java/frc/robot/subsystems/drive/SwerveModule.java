c9717b948f300704476c23e2c2306f1bfeb90df3
TheComputer <thecomputer314@gmail.com>
2023-04-03 14:43:34 -0500

    Implement motor construction

--- src/main/java/frc/robot/subsystems/drive/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/drive/SwerveModule.java
@@ -1,16 +1,18 @@
 package frc.robot.subsystems.drive;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
 
 public class SwerveModule {
     private final Translation2d position;
 
-    private final int driveMotorID;
-    private final int turnMotorID;
+    private final CANSparkMax driveMotor;
+    private final CANSparkMax turnMotor;
 
     public SwerveModule(Translation2d position, int driveMotorID, int turnMotorID) {
         this.position = position;
-        this.driveMotorID = driveMotorID;
-        this.turnMotorID = turnMotorID;
+        this.driveMotor = new CANSparkMax(driveMotorID, CANSparkMaxLowLevel.MotorType.kBrushless);
+        this.turnMotor = new CANSparkMax(turnMotorID, CANSparkMaxLowLevel.MotorType.kBrushless);
     }
 }
