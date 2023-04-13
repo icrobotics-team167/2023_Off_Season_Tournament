a9742899b51bb18309d6eaf8759104e1884c71dd
TheComputer <thecomputer314@gmail.com>
2023-04-13 11:02:26 -0500

    Reimplement motor speed setting

--- src/main/java/frc/robot/subsystems/drive/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/drive/SwerveModule.java
@@ -3,14 +3,15 @@ package frc.robot.subsystems.drive;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import edu.wpi.first.math.controller.*;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.*;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.util.PeriodicTimer;
 
 public class SwerveModule {
     private final CANSparkMax driveMotor;
@@ -19,11 +20,20 @@ public class SwerveModule {
     private final DutyCycleEncoder turnEncoder;
     private final int turnEncoderPort;
 
+    // TODO: Tune PID values
+    private SparkMaxPIDController drivePID;
+    private final double DRIVE_P = 0.25;
+    private final double DRIVE_I = 0;
+    private final double DRIVE_D = 0;
+    private final double DRIVE_FF = 0.025;
+
     private PIDController turnPID;
     private final double TURN_P = 1.0 / 90.0;
     private final double TURN_I = 0;
     private final double TURN_D = 0.5;
-    private PeriodicTimer pidTimer;
+
+    private final double WHEEL_DIAMETER = 4;
+    private final double GEAR_RATIO = 1; // TODO: Figure out module gear ratio
 
     private double distanceDrivenMeters;
 
@@ -32,6 +42,15 @@ public class SwerveModule {
         this.turnMotor = new CANSparkMax(turnMotorID, CANSparkMaxLowLevel.MotorType.kBrushless);
 
         this.driveEncoder = driveMotor.getEncoder();
+        // Convert from "rotations of motor" to "meters driven by wheel"
+        this.driveEncoder.setPositionConversionFactor(WHEEL_DIAMETER * Math.PI / GEAR_RATIO);
+        // Convert from "rotations per minute of motor" to "meters per second of wheel"
+        this.driveEncoder.setVelocityConversionFactor(Units.inchesToMeters(WHEEL_DIAMETER * Math.PI / GEAR_RATIO / 60));
+        this.drivePID = this.driveMotor.getPIDController();
+        this.drivePID.setP(DRIVE_P);
+        this.drivePID.setI(DRIVE_I);
+        this.drivePID.setD(DRIVE_D);
+        this.drivePID.setFF(DRIVE_FF);
 
         this.turnEncoder = new DutyCycleEncoder(turnEncoderDIOPort); // Assumes we're using a absolute encoder
         this.turnEncoderPort = turnEncoderDIOPort;
@@ -41,8 +60,6 @@ public class SwerveModule {
         turnMotor.setSmartCurrentLimit(40);
         turnMotor.setSecondaryCurrentLimit(60);
 
-        pidTimer = new PeriodicTimer();
-        pidTimer.reset();
         turnPID = new PIDController(TURN_P, TURN_I, TURN_D);
 
         distanceDrivenMeters = 0;
@@ -61,13 +78,16 @@ public class SwerveModule {
         turnMotor.set(MathUtil.clamp(turnPID.calculate(getAngle(), state.angle.getDegrees()), -1, 1));
         // Calculate rive motor power from a speed in m/s, and set the motor power to
         // that
-        driveMotor.set(speedMetersPerSecondToMotorPower(state.speedMetersPerSecond));
+        // driveMotor.set(speedMetersPerSecondToMotorPower(state.speedMetersPerSecond));
+        drivePID.setReference(state.speedMetersPerSecond, CANSparkMax.ControlType.kVelocity);
 
         // Calculate distance driven by the module for odometry/kinematics
-        distanceDrivenMeters = motorEncoderToMeters(driveEncoder.getPosition());
+        distanceDrivenMeters = getDistanceDriven();
 
-        SmartDashboard.putNumber("Module " + turnEncoderPort + " current angle", getAngle());
-        SmartDashboard.putNumber("Module " + turnEncoderPort + " desired angle", state.angle.getDegrees());
+        SmartDashboard.putNumber("Module " + turnEncoderPort + " current angle (degrees)", getAngle());
+        SmartDashboard.putNumber("Module " + turnEncoderPort + " desired angle (degrees)", state.angle.getDegrees());
+        SmartDashboard.putNumber("Module " + turnEncoderPort + " current velocity (m/s)", driveEncoder.getVelocity());
+        SmartDashboard.putNumber("Module " + turnEncoderPort + " desired velocity (m/s)", state.speedMetersPerSecond);
     }
 
     public void stop() {
@@ -92,16 +112,7 @@ public class SwerveModule {
         return turnEncoder.getAbsolutePosition();
     }
 
-    private double speedMetersPerSecondToMotorPower(double speedMetersPerSecond) {
-        // TODO: Figure out right scale factor for speedMetersPerSecondToMotorPower
-        final double SCALE_FACTOR = 1;
-        return MathUtil.clamp(speedMetersPerSecond * SCALE_FACTOR, -1, 1);
-    }
-
-    private double motorEncoderToMeters(double encoderValue) {
-        // TODO: Figure out right scale factor for motorEncoderToMeters
-        // Might be redundant if we set setPositionScaleFactor() right
-        final double SCALE_FACTOR = 1;
-        return encoderValue * SCALE_FACTOR;
+    public double getDistanceDriven() {
+        return driveEncoder.getPosition();
     }
 }
