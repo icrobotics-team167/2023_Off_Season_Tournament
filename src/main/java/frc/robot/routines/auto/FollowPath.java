078bb237c701e6fa3a127f0892ddd5fd70a426d3
TheComputer <thecomputer314@gmail.com>
2023-10-20 09:45:09 -0500

    Gutting everything and starting from scratch lol

--- src/main/java/frc/robot/routines/auto/FollowPath.java
+++ src/main/java/frc/robot/routines/auto/FollowPath.java
@@ -5,15 +5,17 @@ import frc.robot.routines.Action;
 import frc.robot.subsystems.Subsystems;
 import frc.robot.util.PeriodicTimer;
 
-import com.pathplanner.lib.PathPlanner;
-import com.pathplanner.lib.PathPlannerTrajectory;
-import com.pathplanner.lib.PathPlannerTrajectory.PathPlannerState;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.path.PathPlannerTrajectory;
+import com.pathplanner.lib.path.PathPlannerTrajectory.State;
 
+import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.math.controller.HolonomicDriveController;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
 public class FollowPath extends Action {
 
@@ -40,43 +42,25 @@ public class FollowPath extends Action {
 
     public FollowPath(String path) {
         super();
-        this.path = PathPlanner.loadPath(path, Config.Settings.SwerveDrive.MAX_MOVE_SPEED, Config.Settings.SwerveDrive.MAX_MOVE_ACCEL); 
-        xController = new PIDController(xP, xI, xD);
-        yController = new PIDController(yP, yI, yD);
-        rotController = new ProfiledPIDController(rotP, rotI, rotD,
-                new TrapezoidProfile.Constraints(Config.Settings.SwerveDrive.MAX_TURN_SPEED, Config.Settings.SwerveDrive.MAX_TURN_ACCEL));
-        driveController = new HolonomicDriveController(xController, yController, rotController);
-        timer = new PeriodicTimer();
     }
 
     @Override
     public void init() {
-        Subsystems.driveBase.setPose(path.getInitialPose());
-        timer.reset();
-        SmartDashboard.putNumber("StartXPos", Subsystems.driveBase.getPose().getX());
-        SmartDashboard.putNumber("StartYPos", Subsystems.driveBase.getPose().getY());
+
     }
 
     @Override
     public void periodic() {
-        PathPlannerState state = (PathPlannerState) path.sample(timer.get());
-        Subsystems.driveBase.fieldOrientedDrive(
-                driveController.calculate(Subsystems.driveBase.getPose(), state, state.poseMeters.getRotation()),
-                Subsystems.gyro.getYaw());
-        SmartDashboard.putNumber("xPosWant", state.poseMeters.getX());
-        SmartDashboard.putNumber("yPosWant", state.poseMeters.getY());
 
     }
 
     @Override
     public boolean isDone() {
-        // return timer.get() >= path.getTotalTimeSeconds();
         return false;
     }
 
     @Override
     public void done() {
-        Subsystems.driveBase.stop();
     }
 
 }
