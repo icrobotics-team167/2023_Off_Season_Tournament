136c08d60d9ec90b0459dbb42846c0f2b650430e
TheComputer314 <thecomputer314@gmail.com>
2023-10-27 16:43:01 -0500

    Fix various bugs

--- src/main/java/frc/robot/routines/auto/SimpleDrive.java
+++ src/main/java/frc/robot/routines/auto/SimpleDrive.java
@@ -13,7 +13,7 @@ public class SimpleDrive extends Action {
     PeriodicTimer timer;
     private TurretPosition targetState = null;
     private boolean turretDone = true;
-
+    private boolean runIntake = false;
 
     public SimpleDrive(ChassisSpeeds speeds, double seconds) {
         this.speeds = speeds;
@@ -21,10 +21,20 @@ public class SimpleDrive extends Action {
         timer = new PeriodicTimer();
     }
 
+    public SimpleDrive withTurret(TurretPosition position) {
+        targetState = position;
+        turretDone = false;
+        return this;
+    }
+
+    public SimpleDrive withIntake() {
+        this.runIntake = true;
+        return this;
+    }
+
     @Override
     public void init() {
         timer.reset();
-        TurretMove(TurretPosition.CONE_HIGH);
     }
 
     @Override
@@ -35,8 +45,13 @@ public class SimpleDrive extends Action {
         } else {
             Subsystems.turret.stop();
         }
-        new ChassisSpeeds driveCommands = null;
-        Subsystems.driveBase.drive(driveCommands);
+
+        if (runIntake) {
+            Subsystems.claw.intake();
+        } else {
+            Subsystems.claw.stop();
+        }
+        Subsystems.driveBase.fieldOrientedDrive(speeds, Subsystems.gyro.getYaw());
     }
 
     @Override
@@ -47,16 +62,5 @@ public class SimpleDrive extends Action {
     @Override
     public void done() {
         Subsystems.driveBase.stop();
-    }
-
-    // //public FollowPath withTurret(TurretPosition targetState) {
-    //     thitargetStates. = targetState;
-    //     this.turretDone = false;
-    //     return this;
-    // }
-    @Override
-    public void turretMove(TurretPosition target){
-        this.targetState = target;
-        this.turretDone = false;
     }
 }
