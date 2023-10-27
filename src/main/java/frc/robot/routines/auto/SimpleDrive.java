3f5233fa0e00012db01f213254232c51e2a08244
RealGenius1 <sptho24@gmail.com>
2023-10-27 16:17:19 -0500

    Implement turret

--- src/main/java/frc/robot/routines/auto/SimpleDrive.java
+++ src/main/java/frc/robot/routines/auto/SimpleDrive.java
@@ -3,6 +3,7 @@ package frc.robot.routines.auto;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import frc.robot.routines.Action;
 import frc.robot.subsystems.Subsystems;
+import frc.robot.subsystems.turret.*;
 import frc.robot.util.PeriodicTimer;
 
 public class SimpleDrive extends Action {
@@ -10,6 +11,9 @@ public class SimpleDrive extends Action {
     ChassisSpeeds speeds;
     double seconds;
     PeriodicTimer timer;
+    private TurretPosition targetState = null;
+    private boolean turretDone = true;
+
 
     public SimpleDrive(ChassisSpeeds speeds, double seconds) {
         this.speeds = speeds;
@@ -20,11 +24,19 @@ public class SimpleDrive extends Action {
     @Override
     public void init() {
         timer.reset();
+        TurretMove(TurretPosition.CONE_HIGH);
     }
 
     @Override
     public void periodic() {
         Subsystems.driveBase.fieldOrientedDrive(speeds, Subsystems.gyro.getYaw());
+        if (targetState != null && !turretDone) {
+            turretDone = Subsystems.turret.moveTo(targetState);
+        } else {
+            Subsystems.turret.stop();
+        }
+        new ChassisSpeeds driveCommands = null;
+        Subsystems.driveBase.drive(driveCommands);
     }
 
     @Override
@@ -36,5 +48,15 @@ public class SimpleDrive extends Action {
     public void done() {
         Subsystems.driveBase.stop();
     }
-    
+
+    // //public FollowPath withTurret(TurretPosition targetState) {
+    //     thitargetStates. = targetState;
+    //     this.turretDone = false;
+    //     return this;
+    // }
+    @Override
+    public void turretMove(TurretPosition target){
+        this.targetState = target;
+        this.turretDone = false;
+    }
 }
