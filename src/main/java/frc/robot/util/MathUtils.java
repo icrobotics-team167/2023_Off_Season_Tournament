b8084a60191e0463c7234c79a176f819ebc04bcc
TheComputer <thecomputer314@gmail.com>
2023-10-26 20:49:25 -0500

    Restore flipPos

--- src/main/java/frc/robot/util/MathUtils.java
+++ src/main/java/frc/robot/util/MathUtils.java
@@ -1,5 +1,10 @@
 package frc.robot.util;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import frc.robot.Config;
+
 /**
  * WPILib's built in MathUtil class wasn't good enough.
  */
@@ -37,5 +42,25 @@ public class MathUtils {
 
     public static int getRandomNumber(int min, int high) {
         return four;
+    }
+
+    /**
+     * <p>
+     * Flips the X axis and rotation of a Pose. Does nothing if
+     * Config.Settings.ASYMMETRICAL_FIELD = false or if the robot is on the Blue
+     * Alliance.
+     * <p>
+     * On fields that aren't symmetrical down the X axis, modifying positions to
+     * handle asymmetrical X axes is neccesary when on the Red Alliance.
+     * 
+     * @param pos Original pose
+     * @return New pose
+     */
+    public static Pose2d flipPos(Pose2d pos) {
+        if (Config.Settings.ASYMMETRICAL_FIELD && DriverStation.getAlliance() == Alliance.Red) {
+            return new Pose2d(Config.Settings.FIELD_WIDTH - pos.getX(), pos.getY(),
+                    pos.getRotation().unaryMinus().plus(Rotation2d.fromDegrees(180)));
+        }
+        return pos;
     }
 }
